9faa86fe9252ac38ec3876e6c0bb2eb7cf3a5e2d
HyeonJeong <wgwjh05169@gmail.com>
2024-05-31 02:35:04 +0900

    Feat: 리뷰 서비스에 리뷰 추천 메소드 구현

--- src/main/java/ggong_ggong/ridingbud/application/ReviewService.java
+++ src/main/java/ggong_ggong/ridingbud/application/ReviewService.java
@@ -1,6 +1,8 @@
 package ggong_ggong.ridingbud.application;
 
+import ggong_ggong.ridingbud.domain.Recommendation;
 import ggong_ggong.ridingbud.domain.Review;
+import ggong_ggong.ridingbud.persistence.RecommendationRepository;
 import ggong_ggong.ridingbud.persistence.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final RecommendationRepository recommendationRepository;
 
     public void enrollReview(CreateReviewCommand command) {
         Review review = Review.from(command);
 
         reviewRepository.save(review);
+    }
+
+    public void recommendReview(RecommendReviewCommand command) {
+        Recommendation recommendation = Recommendation.from(command);
+
+        recommendationRepository.save(recommendation);
     }
 }
