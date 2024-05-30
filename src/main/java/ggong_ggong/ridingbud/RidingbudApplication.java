914c6b754e62a263c6dad168d87c36baba2ee29b
HyeonJeong <wgwjh05169@gmail.com>
2024-05-30 21:04:45 +0900

    Init: 프로젝트 초기화

---
+++ src/main/java/ggong_ggong/ridingbud/RidingbudApplication.java
@@ -0,0 +1,13 @@
+package ggong_ggong.ridingbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidingbudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidingbudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ggong_ggong/ridingbud/RidingbudApplicationTests.java
@@ -0,0 +1,13 @@
+package ggong_ggong.ridingbud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidingbudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
