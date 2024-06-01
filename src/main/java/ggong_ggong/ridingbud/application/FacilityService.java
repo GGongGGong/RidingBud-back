0a605ab18e1be7c42ef742374d404f3a4a8a7119
HyeonJeong <wgwjh05169@gmail.com>
2024-06-01 19:50:44 +0900

    Feat: 시설물 서비스에 공기주입기 조회 메소드 구현

--- src/main/java/ggong_ggong/ridingbud/application/FacilityService.java
+++ src/main/java/ggong_ggong/ridingbud/application/FacilityService.java
@@ -1,7 +1,9 @@
 package ggong_ggong.ridingbud.application;
 
 import ggong_ggong.ridingbud.domain.BicycleRental;
+import ggong_ggong.ridingbud.domain.PumpStation;
 import ggong_ggong.ridingbud.persistence.BicycleRentalRepository;
+import ggong_ggong.ridingbud.persistence.PumpStationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FacilityService {
     private final BicycleRentalRepository bicycleRentalRepository;
+    private final PumpStationRepository pumpStationRepository;
 
     public List<BicycleRental> getBicycleRentals() {
         return bicycleRentalRepository.findAll();
+    }
+
+    public List<PumpStation> getPumpStations() {
+        return pumpStationRepository.findAll();
     }
 }
