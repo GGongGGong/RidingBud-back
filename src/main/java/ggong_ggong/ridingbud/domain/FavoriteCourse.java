0a64144e5572fe6bf756a39ea6429c4e8e05340d
bokyeong <wntjdals0412@gmail.com>
2024-06-01 19:50:18 +0900

    Feat: FavoriteCourse 도메인

---
+++ src/main/java/ggong_ggong/ridingbud/domain/FavoriteCourse.java
@@ -0,0 +1,23 @@
+package ggong_ggong.ridingbud.domain;
+
+import jakarta.persistence.*;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@RequiredArgsConstructor
+public class FavoriteCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long favoriteCourseId;
+
+    @JoinColumn(name = "course_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Course course;
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private User user;
+}
